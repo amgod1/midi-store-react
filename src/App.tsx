867063252fe5863d72db66ab1646ef6cadaa8291
kamill <kamillschepuk@gmail.com>
2022-10-26 00:07:56 +0300

    made it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Store from './pages/Store'
-import About from './pages/About'
+// import About from './pages/About'
 import Navbar from './components/Navbar'
 import { ShoppingCartProvider } from './context/ShoppingCartContext'
 
@@ -10,10 +10,11 @@ function App() {
     <ShoppingCartProvider>
       <Navbar />
       <Container className='mb-4'>
-        <Routes>
+        <Store />
+        {/* <Routes>
          <Route path="/" element={<Store />} />
          <Route path="/about" element={<About />} />
-        </Routes>
+        </Routes> */}
       </Container>
     </ShoppingCartProvider>
   )
